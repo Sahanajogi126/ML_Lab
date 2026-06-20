69bba00ce2147e4966d4947724c57a32fff91ccb
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2026-06-20 08:25:36 +0530

    Update ML_1.py

--- ML_1.py
+++ ML_1.py
@@ -2,35 +2,39 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.datasets import fetch_california_housing
-data=fetch_california_housing(as_frame=True)
-#print(data.frame.head())
+from sklearn.datasets import fetch_california_housing# Step 1: Load the California Housing dataset
+data = fetch_california_housing(as_frame=True)
 housing_df=data.frame
-numerical_features=housing_df.select_dtypes(include=[np.number]).columns
+numerical_features=housing_df.select_dtypes(include=[np.number])
+numerical_features.head()
+#plot_histogram
 plt.figure(figsize=(15,10))
-for i,feature in enumerate(numerical_features):
+for i ,feature in enumerate(numerical_features.columns):
   plt.subplot(3,3,i+1)
-  sns.histplot(housing_df[feature],kde=True,bins=30,color='blue')
-  plt.title(f'Distribution of {feature}')
+  sns.histplot(data=housing_df[feature],kde=True,bins=45,color='blue')
+  plt.title(f'Histogram of {feature}')
 plt.tight_layout()
 plt.show()
+
+#plot_boxplot
 plt.figure(figsize=(15,10))
-for i,feature in enumerate(numerical_features):
+for i ,feature in enumerate(numerical_features.columns):
   plt.subplot(3,3,i+1)
-  sns.boxplot(x=housing_df[feature],color='orange')
+  sns.boxplot(data=housing_df[feature],color='orange')
   plt.title(f'Box Plot of {feature}')
 plt.tight_layout()
 plt.show()
-print("Outliers Detection:")
+
+#identify_outliers
+print("Description of Outliers")
 outliers_summary={}
-for feature in numerical_features:
+for feature in numerical_features.columns:
   Q1=housing_df[feature].quantile(0.25)
   Q3=housing_df[feature].quantile(0.75)
   IQR=Q3-Q1
   lower_bound=Q1-1.5*IQR
   upper_bound=Q3+1.5*IQR
-  outliers=housing_df[(housing_df[feature]<lower_bound)|(housing_df[feature]>upper_bound)]
+  outliers=housing_df[(housing_df[feature]<lower_bound) | (housing_df[feature]>upper_bound)]
   outliers_summary[feature]=len(outliers)
-  print(f"{feature}:{len(outliers)}outliers")
-print("\nDataset Summary:")
+print(outliers_summary)
 print(housing_df.describe())
